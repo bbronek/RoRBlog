146342bc82f4587b3f5413f1b10e6239fc33cdba
bronek <barbro214@gmail.com>
2021-05-02 17:48:54 +0200

    Add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_234121) do
+ActiveRecord::Schema.define(version: 2021_05_02_152253) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
